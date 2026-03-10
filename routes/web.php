70f7ffa694b450dd8faa4bc7c2c999a29ea0de5f
stephanie <222stephasantapiez@gmail.com>
2026-03-10 10:44:00 -0500

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/consultar', function (){
+    $user = new App\Models\User();
+    return dd($user->all());
+});
+
+Route::get('/insertar', function (){
+    $user = new App\Models\User();
+    $user->email = 'email@gmail.com';
+    $user->name = 'Ejemplo 2';
+    $user->password = "mypassword";
+    $user->save();
+    return dd($user);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
